d41e61eef9b5b4cae6456e91848e90747078b5c6
kamaslau <kamaslau@outlook.com>
2019-12-25 03:20:48 +0800

    beta20191225
    Added
    保存数据到API
    
    TODO
    提示API操作结果

--- js/background.js
+++ js/background.js
@@ -7,3 +7,79 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.bookmarks.onCreated.addListener(() => {
     alert('bookmark saved')
 })
+
+// 内部通信
+chrome.runtime.onMessage.addListener(
+	async (request, sender, sendResponse) => {
+        // 保存数据
+        if (request.action === 'saveData') {
+            // console.log(request.params)
+
+            await api_request(request.params)
+            .then(
+                data => {
+                    console.log(data)
+                    sendResponse(data)
+                }
+            )
+        }
+	}
+)
+
+// API公共参数
+const common_params = {
+    'app_type': 'biz',
+    'operator_id': 1
+}
+// API请求参数
+let params = {}
+
+// 请求方法
+const api_request = async (inputs) => {
+    // 请求API
+    const params = {
+        ...common_params,
+        ...inputs
+    }
+
+    // 将参数转为FormData进行发送
+    const form_data = new FormData()
+    Object.keys(params).forEach(
+        (key) => {
+            form_data.append(key, params[key])
+            // console.log(form_data.get(key)) // FormData对象无法直接console.log，可通过get方法查看特定键值
+        }
+    )
+
+    let message = ''
+
+    await new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+
+        request.open('post', 'https://api.imakp.liuyajie.com/item_template/create', true)
+        
+        request.onload = () => {
+            
+            if (request.status === 200) {
+                const result = JSON.parse(request.response)
+                // console.log(result)
+                if (result.status === 200) {
+                    message = result.content.message
+                }
+                else {
+                    message = result.content.error.message
+                }
+                resolve(message)
+            }
+            else {
+                reject(Error(request.statusText))
+            }
+        }
+        request.onerror = () => {
+            reject(Error('Network Error'))
+        }
+        request.send(form_data)
+    })
+
+    return message // end Promise
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 console.log('popup.js loaded')
 
-let current_tab
+let current_tab // 当前页面（即浏览器标签页）信息
+const store = {
+    'name': '', // 标题
+    'tag_price': 0.00, // 标签价/原价
+    'barcode': '' // 条形码/ISBN
+}
+
 window.onload = () => {
     current_tab = getCurrentTab()
 }
@@ -9,7 +15,7 @@ window.onload = () => {
 // https://developer.chrome.com/extensions/tabs#method-query
 const getCurrentTab = () => {
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-        // console.log(tabs[0])
+        // console.log(tabs)
         current_tab = tabs[0]
     })
 
@@ -25,13 +31,34 @@ getBrief.addEventListener('click', () => {
         current_tab.id,
         {action: "getBrief"},
         function(response) {
-            console.log(response)
-            document.getElementById('title').value = response.title
-            document.getElementById('tag_price').value = response.tag_price
-            document.getElementById('barcode').value = response.barcode
+            // console.log(response)
+            store.name = document.getElementById('title').value = response.title
+            store.tag_price = document.getElementById('tag_price').value = response.tag_price
+            store.barcode = document.getElementById('barcode').value = response.barcode
         }
     )
 })
+
+/**
+ * 保存数据
+ * 
+ * 发送数据到API
+ */
+const saveData = document.querySelector('#saveData')
+saveData.addEventListener('click', async () => {
+    // 发送到插件后台（js/background.js）进行API请求
+    await chrome.runtime.sendMessage(
+        {
+            action: 'saveData',
+            params: { ...store }
+        },
+        (response) => {
+            console.log('response to saveData: ', response)
+            alert(response)
+        }
+    )
+})
+
 
 // 获取商品标题
 const getTitle = document.querySelector('#getTitle')
@@ -40,9 +67,9 @@ getTitle.addEventListener('click', () => {
 
     chrome.tabs.sendMessage(
         current_tab.id,
-        {action: "getTitle"},
-        function(response) {
-            console.log(response)
+        {action: 'getTitle'},
+        (response) => {
+            // console.log(response)
             document.getElementById('title').value = response
         }
     )
@@ -55,9 +82,9 @@ getTagPrice.addEventListener('click', () => {
 
     chrome.tabs.sendMessage(
         current_tab.id,
-        {action: "getTagPrice"},
-        function(response) {
-            console.log(response)
+        {action: 'getTagPrice'},
+        (response) => {
+            // console.log(response)
             document.getElementById('tag_price').value = response
         }
     )
@@ -70,9 +97,9 @@ getBarcode.addEventListener('click', () => {
 
     chrome.tabs.sendMessage(
         current_tab.id,
-        {action: "getBarcode"},
-        function(response) {
-            console.log(response)
+        {action: 'getBarcode'},
+        (response) => {
+            // console.log(response)
             document.getElementById('barcode').value = response
         }
     )
